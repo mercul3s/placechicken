4942235c697c6645ee55222bf40b98febbd834ec
Mercedes Coyle <me@mercedescoyle.com>
2019-06-24 15:38:46 -0400

    Update grid css and index template
    Ensure content and images line up properly on the page. Remove image
    header in favor of text header.

--- placechicken.go
+++ placechicken.go
@@ -15,11 +15,10 @@ func main() {
 	static := os.Getenv("STATIC")
 	resized := os.Getenv("RESIZED")
 	p := placer.Config(static, resized)
+	logger.Printf("placechicken started with directories static: %s and resized: %s", static, resized)
 	m := router.NewMux(p, "static/", "templates/")
 	err := http.ListenAndServe(":8888", m.Router)
 	if err != nil {
 		logger.Print(err)
-	} else {
-		logger.Print("placechicken started")
 	}
 }
